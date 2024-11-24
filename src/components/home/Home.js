4fc85c4ef0bbca19e3f66f946a429cc1d300caf8
ahsanch007 <cahsan562@gmail.com>
2024-11-24 13:43:26 +0500

    done page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,9 +3,11 @@ import { StorageSection } from './StorageSection';
 import { UspsSection } from './UspsSection';
 import { FactsSection } from './FactsSection';
 import { FaqSection } from './FaqSection';
+import DynamicCarDetailsForm from './Calculator';
 export const Home = () => {
   return (
     <div>
+      <DynamicCarDetailsForm />
       <StorageSection />
       <UspsSection />
       <FactsSection />
