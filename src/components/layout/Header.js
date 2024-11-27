73b0edadef22be5dcfa5534c2b837b13735c56ad
WAJDAN-KHAN <ahsan.ali290620@gmail.com>
2024-11-27 21:10:24 +0500

    header remoce

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,33 +8,7 @@ export const Header = () => {
           <div className="logo p-[15px]">
             <img src="/images/logo.png" className="lg:max-w-[200px] w-full h-auto  max-w-[150px]" alt="" />
           </div>
-          <ul className="md:flex hidden items-center gap-3">
-            <li>
-              <a href="#" className="p-[15px] text-white hover:bg-white transition-all hover:text-[#ee202c]">
-                Auto verkaufen
-              </a>
-            </li>
-            <li>
-              <a href="#" className="p-[15px] text-white hover:bg-white transition-all hover:text-[#ee202c]">
-                Auto bewerten
-              </a>
-            </li>
-            <li>
-              <a href="#" className="p-[15px] text-white hover:bg-white transition-all hover:text-[#ee202c]">
-                Auto kaufen
-              </a>
-            </li>
-            <li>
-              <a href="#" className="p-[15px] text-white hover:bg-white transition-all hover:text-[#ee202c]">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="#" className="p-[15px] text-white hover:bg-white transition-all hover:text-[#ee202c]">
-                Über uns
-              </a>
-            </li>
-          </ul>
+
           <div className="md:hidden">
             <img src="/images/menu.png" className="cursor-pointer" alt="" />
           </div>
